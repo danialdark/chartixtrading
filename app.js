262d1dacc503b7a00a597b5deebc5e04621d2bea
Danial <danialdark12098@gmail.com>
2023-11-09 12:12:09 +0330

    just for test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 3000;
 const redis = new Redis({
     host: 'localhost',
     port: '6379',
-    password: 'D@n!@l12098',
+    password: '',
     enableCompression: true,
 });
 var pipeline = redis.pipeline();
